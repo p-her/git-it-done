22a177bd7e893d7f6b8f0b93069d66c6661fdc92
Pao Her <p.her007@hotmail.com>
2022-01-05 15:55:11 -0600

    call the api functions when buttons are clicked

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,6 +3,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonEl = document.querySelector("#language-buttons");
 
 var getUserRepos = function(user) {
 
@@ -85,4 +86,30 @@ var displayRepos = function(repos, searchTerm){
     }
 };
 
+var getFeaturedRepos = function(language) { 
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+    fetch(apiUrl).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                displayRepos(data.items, language)
+            })
+            console.log(response)
+        }else{
+            alert("Error: GitHub User Not Found")
+        }
+    })
+}
+
+
+var buttonClickHandler = function(event){
+    var language = event.target.getAttribute("data-language");
+    
+    if(language){
+        getFeaturedRepos(language);
+
+        repoContainerEl.textContent = "";
+    }
+
+}
+languageButtonEl.addEventListener("click", buttonClickHandler);
 userFormEl.addEventListener("submit", formSubmitHandler);
